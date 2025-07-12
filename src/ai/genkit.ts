aa5a9ab2878c8e37e738bc0cb4ae8d5f023c5ab3
ROHIT Antil JAAT <rohit9992030286@gmail.com>
2025-07-12 13:59:33 +0000

    GenkitError: FAILED_PRECONDITION: Please pass in the API key or set the

--- src/ai/genkit.ts
+++ src/ai/genkit.ts
@@ -1,7 +1,14 @@
-import {genkit} from 'genkit';
+import {genkit, Plugin} from 'genkit';
 import {googleAI} from '@genkit-ai/googleai';
+import {noop} from 'genkit/noop';
+
+const geminiApiKey = process.env.GEMINI_API_KEY;
+
+const plugins: Plugin<any, any>[] = [
+  geminiApiKey ? googleAI({apiKey: geminiApiKey}) : noop('google-ai'),
+];
 
 export const ai = genkit({
-  plugins: [googleAI({apiKey: process.env.GEMINI_API_KEY})],
-  model: 'googleai/gemini-2.0-flash',
+  plugins: plugins,
+  model: geminiApiKey ? 'googleai/gemini-2.0-flash' : undefined,
 });
