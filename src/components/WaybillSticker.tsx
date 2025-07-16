da5e1f3cbbf314f74c8ebc8c792ed2ab380e37a8
ROHIT Antil JAAT <rohit9992030286@gmail.com>
2025-07-16 18:03:42 +0000

    and your created barcoad is not scaning make it to scan easyly on all de

--- src/components/WaybillSticker.tsx
+++ src/components/WaybillSticker.tsx
@@ -3,6 +3,7 @@
 
 import { Waybill } from '@/types/waybill';
 import { cn } from '@/lib/utils';
+import Barcode from 'react-barcode';
 
 interface WaybillStickerProps {
   waybill: Waybill;
@@ -10,14 +11,6 @@ interface WaybillStickerProps {
   boxNumber?: number;
   totalBoxes?: number;
 }
-
-const Barcode = () => (
-    <svg xmlns="http://www.w3.org/2000/svg" width="100%" height="100%" viewBox="0 0 320 60" preserveAspectRatio="none">
-        <g fill="#000">
-        <path d="M0 0h3v60H0z M5 0h5v60H5z M12 0h3v60h-3z M17 0h3v60h-3z M22 0h7v60h-7z M31 0h3v60h-3z M38 0h3v60h-3z M43 0h5v60h-5z M50 0h3v60h-3z M57 0h3v60h-3z M62 0h5v60h-5z M71 0h3v60h-3z M76 0h7v60h-7z M87 0h5v60h-5z M94 0h3v60h-3z M101 0h3v60h-3z M106 0h3v60h-3z M113 0h5v60h-5z M120 0h3v60h-3z M125 0h7v60h-7z M134 0h3v60h-3z M139 0h5v60h-5z M146 0h3v60h-3z M153 0h5v60h-5z M160 0h3v60h-3z M165 0h7v60h-7z M174 0h5v60h-5z M181 0h3v60h-3z M188 0h3v60h-3z M193 0h7v60h-7z M202 0h3v60h-3z M209 0h5v60h-5z M216 0h3v60h-3z M221 0h5v60h-5z M230 0h3v60h-3z M235 0h3v60h-3z M242 0h5v60h-5z M249 0h7v60h-7z M258 0h3v60h-3z M263 0h5v60h-5z M270 0h3v60h-3z M277 0h7v60h-7z M286 0h5v60h-5z M292 0h3v60h-3z M297 0h7v60h-7z M306 0h5v60h-5z M313 0h7v60h-7z" />
-        </g>
-    </svg>
-);
 
 export function WaybillSticker({ waybill, stickerSize = '75mm', boxNumber, totalBoxes }: WaybillStickerProps) {
   
@@ -34,8 +27,14 @@ export function WaybillSticker({ waybill, stickerSize = '75mm', boxNumber, total
     <div className={cn(baseClasses, sizeClasses[stickerSize as keyof typeof sizeClasses] || sizeClasses['75mm'])}>
         {/* Top: Barcode and Waybill Number */}
         <div className="text-center border-b-2 border-dashed border-black pb-2">
-            <div className={cn("mx-auto", stickerSize === '75mm' ? 'w-full h-[40px]' : 'w-full h-[50px]')}>
-                <Barcode />
+            <div className={cn("mx-auto", stickerSize === '75mm' ? 'w-full h-auto' : 'w-full h-auto')}>
+                <Barcode 
+                  value={waybill.waybillNumber} 
+                  height={stickerSize === '75mm' ? 40 : 50} 
+                  displayValue={false} 
+                  width={2} 
+                  margin={0}
+                />
             </div>
             <p className={cn("text-center font-mono", stickerSize === '75mm' ? 'tracking-[0.2em] text-lg' : 'tracking-[0.25em] text-xl')}>{waybill.waybillNumber}</p>
         </div>
