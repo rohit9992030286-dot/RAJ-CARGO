e2e8758c83c77a804a286dacdf17633581a82582
ROHIT Antil JAAT <rohit9992030286@gmail.com>
2025-07-19 15:45:05 +0000

    AND MAKE A BOUNDERY OF PRINT STICKER SHOW THE PRINT LOOK PROOFASNOOL IN

--- src/components/WaybillSticker.tsx
+++ src/components/WaybillSticker.tsx
@@ -14,12 +14,12 @@ interface WaybillStickerProps {
 export function WaybillSticker({ waybill, boxNumber, totalBoxes }: WaybillStickerProps) {
   
   const sizeClasses = 'w-[75mm] h-[75mm] p-2';
-  const baseClasses = "bg-white text-black font-sans flex flex-col border-2 border-dashed border-black print:border-none print:shadow-none";
+  const baseClasses = "bg-white text-black font-sans flex flex-col border-2 border-black print:border-2 print:shadow-none";
 
   return (
     <div className={cn(baseClasses, sizeClasses)}>
         {/* Top: Barcode and Waybill Number */}
-        <div className="text-center border-b-2 border-dashed border-black pb-2">
+        <div className="text-center border-b-2 border-black pb-2">
             <div className={cn("mx-auto", 'w-full h-auto')}>
                 <Barcode 
                   value={waybill.waybillNumber} 
@@ -33,7 +33,7 @@ export function WaybillSticker({ waybill, boxNumber, totalBoxes }: WaybillSticke
         </div>
 
         {/* Middle: Sender City */}
-        <div className="flex-grow flex flex-col items-center justify-center border-b-2 border-dashed border-black">
+        <div className="flex-grow flex flex-col items-center justify-center border-b-2 border-black">
             <p className="text-xs uppercase text-gray-500">From</p>
             <p className={cn("font-black tracking-tighter leading-none", 'text-4xl')}>{(waybill.senderCity || '').toUpperCase()}</p>
         </div>
@@ -46,7 +46,7 @@ export function WaybillSticker({ waybill, boxNumber, totalBoxes }: WaybillSticke
                  <p className="text-sm font-semibold">{waybill.receiverName}</p>
             </div>
             {boxNumber && totalBoxes && (
-                <div className="text-center p-2 border-l-2 border-dashed border-black pl-4">
+                <div className="text-center p-2 border-l-2 border-black pl-4">
                     <p className="text-xs uppercase text-gray-500">Box</p>
                     <p className={cn("font-black", 'text-3xl')}>{boxNumber}/{totalBoxes}</p>
                 </div>
